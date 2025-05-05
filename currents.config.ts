84805a4ecf18bdb354218054a24e3e1e3fce4a75
Krishna Suravarapu <36037520+KrishnaSuravarapu@users.noreply.github.com>
2025-05-05 17:42:56 +0530

    Chore: Some changes

--- currents.config.ts
+++ currents.config.ts
@@ -3,6 +3,7 @@ import { CurrentsConfig } from '@currents/playwright';
 const config: CurrentsConfig = {
   recordKey: '0E1SjVuESGNYCmwO',
   projectId: '1BiNnG',
+  ciBuildId: 'ci-build-id'
 };
 
 export default config;
