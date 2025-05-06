9a939d5f95d97545fb261462f9649c1d6f0daee4
Krishna Suravarapu <36037520+KrishnaSuravarapu@users.noreply.github.com>
2025-05-06 14:31:41 +0530

    Chore: Pushing nx yml

--- apps/e2e-03/src/e2e-03-1.spec.ts
+++ apps/e2e-03/src/e2e-03-1.spec.ts
@@ -2,5 +2,5 @@ import { expect, test } from '@playwright/test';
 
 test('e2e-03-1', async () => {
   console.log(`executing e2e-03-1`);
-  expect(1).toBe(1);
+  expect(1).toBe(2);
 });
--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
 import { currentsReporter } from '@currents/playwright';
 import currentsConfig from './currents.config';
 
-export const reporter = [currentsReporter(currentsConfig)];
+export const reporter = ['html', currentsReporter(currentsConfig)];
