c63c49d10abca39bc66693213df4f0b5dff83071
Krishna Suravarapu <36037520+KrishnaSuravarapu@users.noreply.github.com>
2025-05-06 15:01:28 +0530

    Chore: Removing currents

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,1 @@
-import { currentsReporter } from '@currents/playwright';
-import currentsConfig from './currents.config';
-
-export const reporter = [['html'], currentsReporter(currentsConfig)];
+export const reporter = [['json', { outputFile: 'report.json' }]];
